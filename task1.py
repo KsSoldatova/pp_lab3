8112fd3e5a1df0c4dc5c7934b4bd284e7b1269f3
KsSoldatova <ks.gum19@gmai.com>
2022-12-18 17:37:32 +0400

    some fix

--- task1.py
+++ task1.py
@@ -2,19 +2,25 @@ import csv
 import os
 
 
-def create_csv_annotation(class_name: str, annotation_name: str) -> None:
+def create_csv_annotation(class_name1: str, class_name2: str, annotation_name: str) -> None:
     """This function creates csv annotation with 3 parameters: absolute path to file,
     relative path to file and class of file"""
-    path_to_class = os.path.join('dataset', class_name)
-    class_names = os.listdir(path_to_class)
+    path_to_class1 = os.path.join('dataset', class_name1)
+    class_names1 = os.listdir(path_to_class1)
     with open(annotation_name, mode="w", encoding="UTF-16", newline='') as file:
         file_writer = csv.writer(file, delimiter=",")
-        for name in class_names:
+        for name in class_names1:
             file_writer.writerow(
-                [os.path.abspath(name), os.path.join(path_to_class, name), class_name])
-            print('ready')
+                [os.path.abspath(name), os.path.join(path_to_class1, name), class_name1])
+    path_to_class2 = os.path.join('dataset', class_name2)
+    class_names2 = os.listdir(path_to_class2)
+    with open(annotation_name, mode="a", encoding="UTF-16", newline='') as file:
+        file_writer = csv.writer(file, delimiter=",")
+        for name in class_names2:
+            file_writer.writerow(
+                [os.path.abspath(name), os.path.join(path_to_class2, name), class_name2])
 
 
-def run1(class_name: str, annotation_name: str) -> None:
+def run1(class_name1: str, class_name2: str, annotation_name: str) -> None:
     """This function calls previous function in main"""
-    create_csv_annotation(class_name, annotation_name)
+    create_csv_annotation(class_name1, class_name2, annotation_name)
